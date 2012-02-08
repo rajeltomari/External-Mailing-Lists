8d6b1146f87c206dd07cace61ff2017e5a8b0161
Dustin <demonicpagan@gmail.com>
2012-02-07 19:04:23 -0600

    Updated MOD for Nova2 compatibility.
    Signed-off-by: Dustin <demonicpagan@gmail.com>

--- README.md
+++ README.md
@@ -7,7 +7,60 @@ This application is developed under the licenses of Nova and CodeIgniter.
 
 Install Instructions
 --------------------
+The following application will alter your Nova installation to handle external mailing lists. To install this
+application you need to perform the following steps.
 
+*Note: This at this moment assumes that the external mailing list you are using allows anyone to post to alleviate
+the need to add each one of your members of your group to your mailing list. However, if you opt to go this route,
+that is totally your choice and you are free to do so. You can read how this MOD started here:
+<http://forums.anodyne-productions.com/viewtopic.php?f=62&t=2806>
+
+1. Log into your Nova installation.
+
+2. Goto Site Management > Settings.
+
+3. Click "Manage User-Created Settings &raquo;"
+
+4. Click "Add User-Created Setting"
+
+5. Fill in the Label text box and Setting Key text box.
+   Example (the setting key in this example is what is used in write.php):
+     Label: External Mailing List
+	 Setting Key: external_mailing_list (this is only an example, make this whatever you want, just remember it for
+	 use in write.php)
+
+	(You could create 3 different settings for the different kinds of posts if you want, just repeat steps 4 and 5 as
+	needed.)
+
+6. Upload application/controllers/write.php to your application/controllers folder of your Nova install replacing 
+the existing one if you haven't already modified this file. If you already have changes in this file, it's best 
+that you just take the contents of this file and add it into your existing write.php file.
+
+***UCIP MEMBERS***<br />
+You will need to modify the application/controllers/write.php to avoid getting double emails (one from Nova, one from
+UCIP mailing list).
+
+Comment out every occurance of $this->email->cc($this->settings->get_setting('external_mailing_list');
+
+Change the following lines
+
+<table>
+	<tr>
+		<th>Line</th><th>From</th><th>To</th>
+	</tr>
+	<tr>
+		<td>70</td><td>$this->email->to($to);</td>
+		<td>$this->email->to($this->settings->get_setting('external_mailing_list'));</td>
+	</tr>
+	<tr>
+		<td>154</td><td>$this->email->to($to);</td>
+		<td>$this->email->to($this->settings->get_setting('external_mailing_list'));</td>
+	</tr>
+	<tr>
+		<td>256</td><td>$this->email->to($to);</td>
+		<td>$this->email->to($this->settings->get_setting('external_mailing_list'));</td>
+	</tr>
+</table>
 
 If you experience any issues please submit a bug report on
 <http://github.com/demonicpagan/Nova-External-Mailing-Lists/issues>.
@@ -16,4 +69,23 @@ You can always get the latest source from <http://github.com/demonicpagan/Nova-E
 
 Changelog - Dates are in Epoch time
 -----------------------------------
+1328661075:
 
+*	Updating files to be compatible with Nova 2.0.1
+
+1294316085:
+
+*	Updated the _email function to version 1.2.2 of Nova
+
+1284469613:
+
+*	Made sure that the code for the write controller was up to par with Nova 1.1.
+
+1272510152:
+
+*	Created a more readable README for GitHub.
+
+1270020606:
+
+*	Started work with external email lists. This will submit news, logs, and posts to an external mailing list that 
+you have set up in your Nova settings.
---
+++ application/controllers/write.php
@@ -0,0 +1,430 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once MODPATH.'core/controllers/nova_write.php';
+
+class Write extends Nova_write {
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	protected function _email($type, $data)
+	{
+		// load the resources
+		$this->load->library('email');
+		$this->load->library('parser');
+
+		// define the variables
+		$email = false;
+
+		switch ($type)
+		{
+			case 'news':
+			// set some variables
+				$from_name = $this->char->get_character_name($data['author'], true, true);
+				$from_email = $this->user->get_email_address('character', $data['author']);
+				$subject = $data['category'] .' - '. $data['title'];
+
+				// set the content
+				$content = sprintf(
+					lang('email_content_news_item'),
+					$from_name,
+					$data['content']
+				);
+
+				// set the email data
+				$email_data = array(
+					'email_subject' => $subject,
+					'email_content' => ($this->email->mailtype == 'html') ? nl2br($content) : $content
+				);
+
+				// where should the email be coming from
+				$em_loc = Location::email('write_newsitem', $this->email->mailtype);
+
+				// parse the message
+				$message = $this->parser->parse_string($em_loc, $email_data, true);
+
+				// get the email addresses
+				$emails = $this->user->get_crew_emails(true, 'email_news_items');
+
+				// make a string of email addresses
+				$to = implode(',', $emails);
+
+				// set the parameters for sending the email
+				//$this->email->from($from_email, $from_name);
+				$this->email->from(Util::email_sender(), $from_name);
+				$this->email->to($to);
+				$this->email->cc($this->settings->get_setting('external_mailing_list'));
+				$this->email->subject($this->options['email_subject'] .' '. $subject);
+				$this->email->message($message);
+			break;
+
+			case 'news_pending':
+				// set some variables
+				$from_name = $this->char->get_character_name($data['author'], true, true);
+				$from_email = $this->user->get_email_address('character', $data['author']);
+				$subject = $data['category'] .' - '. $data['title'];
+
+				// set the content
+				$content = sprintf(
+					lang('email_content_entry_pending'),
+					lang('global_newsitem'),
+					$data['title'],
+					$from_name,
+					lang('global_newsitem'),
+					$data['content'],
+					lang('global_newsitem'),
+					site_url('login/index')
+				);
+
+				// set the email data
+				$email_data = array(
+					'email_subject' => $subject,
+					'email_content' => ($this->email->mailtype == 'html') ? nl2br($content) : $content
+				);
+
+				// where should the email be coming from
+				$em_loc = Location::email('entry_pending', $this->email->mailtype);
+
+				// parse the message
+				$message = $this->parser->parse_string($em_loc, $email_data, true);
+
+				// get the email addresses
+				$emails = $this->user->get_crew_emails(true, 'email_news_items');
+
+				// make a string of email addresses
+				$to = implode(',', $this->user->get_emails_with_access('manage/news', 2));
+
+				// set the parameters for sending the email
+				$this->email->from(Util::email_sender(), $from_name);
+				$this->email->to($to);
+				$this->email->subject($this->options['email_subject'] .' '. lang('email_subject_news_pending'));
+				$this->email->message($message);
+			break;
+
+			case 'log':
+				// set some variables
+				$from_name = $this->char->get_character_name($data['author'], true, true);
+				$from_email = $this->user->get_email_address('character', $data['author']);
+				$subject = $from_name ."'s ". lang('email_subject_personal_log') ." - ". $data['title'];
+
+				// set the content
+				$content = sprintf(
+					lang('email_content_personal_log'),
+					$from_name,
+					$data['content']
+				);
+
+				// set the email data
+				$email_data = array(
+					'email_subject' => $subject,
+					'email_content' => ($this->email->mailtype == 'html') ? nl2br($content) : $content
+				);
+
+				// where should the email be coming from
+				$em_loc = Location::email('write_personallog', $this->email->mailtype);
+
+				// parse the message
+				$message = $this->parser->parse_string($em_loc, $email_data, true);
+
+				// get the email addresses
+				$emails = $this->user->get_crew_emails(true, 'email_personal_logs');
+
+				// make a string of email addresses
+				$to = implode(',', $emails);
+
+				// set the parameters for sending the email
+				$this->email->from(Util::email_sender(), $from_name);
+				$this->email->to($to);
+				$this->email->cc($this->settings->get_setting('external_mailing_list'));
+				$this->email->subject($this->options['email_subject'] .' '. $subject);
+				$this->email->message($message);
+			break;
+
+			case 'log_pending':
+				// set some variables
+				$from_name = $this->char->get_character_name($data['author'], true, true);
+				$from_email = $this->user->get_email_address('character', $data['author']);
+				$subject = $from_name ."'s ". lang('email_subject_personal_log') ." - ". $data['title'];
+
+				// set the content
+				$content = sprintf(
+					lang('email_content_entry_pending'),
+					lang('global_personallog'),
+					$data['title'],
+					$from_name,
+					lang('global_personallog'),
+					$data['content'],
+					lang('global_personallog'),
+					site_url('login/index')
+				);
+
+				// set the email data
+				$email_data = array(
+					'email_subject' => $subject,
+					'email_from' => $from_name,
+					'email_content' => ($this->email->mailtype == 'html') ? nl2br($content) : $content
+				);
+
+				// where should the email be coming from
+				$em_loc = Location::email('entry_pending', $this->email->mailtype);
+
+				// parse the message
+				$message = $this->parser->parse_string($em_loc, $email_data, true);
+
+				// get the email addresses
+				$to = implode(',', $this->user->get_emails_with_access('manage/logs', 2));
+
+				// set the parameters for sending the email
+				$this->email->from(Util::email_sender(), $from_name);
+				$this->email->to($to);
+				$this->email->subject($this->options['email_subject'] .' '. lang('email_subject_log_pending'));
+				$this->email->message($message);
+			break;
+
+			case 'post':
+				// set some variables
+				$subject = $data['mission'] ." - ". $data['title'];
+				$mission = lang('email_content_post_mission') . $data['mission'];
+				$authors = lang('email_content_post_author') . $this->char->get_authors($data['authors'], true);
+				$timeline = lang('email_content_post_timeline') . $data['timeline'];
+				$location = lang('email_content_post_location') . $data['location'];
+
+				// get an array of authors
+				$authorsArr = explode(',', $data['authors']);
+
+				// find out what's the same
+				$same = array_values(array_intersect($authorsArr, $this->session->userdata('characters')));
+
+				// figure out who it should come from
+				$from = (in_array($this->session->userdata('main_char'), $same)) ? $this->session->userdata('main_char') : $same[0];
+
+				// set who the email is coming from
+				$from_name = $this->char->get_character_name($from, true, true);
+				$from_email = $this->user->get_email_address('character', $from);
+
+				// set the content
+				$content = sprintf(
+					lang('email_content_mission_post'),
+					$authors,
+					$mission,
+					$location,
+					$timeline,
+					$data['content']
+				);
+
+				// set the email data
+				$email_data = array(
+					'email_content' => ($this->email->mailtype == 'html') ? nl2br($content) : $content
+				);
+
+				// where should the email be coming from
+				$em_loc = Location::email('write_missionpost', $this->email->mailtype);
+
+				// parse the message
+				$message = $this->parser->parse_string($em_loc, $email_data, true);
+
+				// get the email addresses
+				$emails = $this->user->get_crew_emails(true, 'email_mission_posts');
+
+				// make a string of email addresses
+				$to = implode(',', $emails);
+
+				// set the parameters for sending the email
+				$this->email->from(Util::email_sender(), $from_name);
+				$this->email->to($to);
+				$this->email->cc($this->settings->get_setting('external_mailing_list'));
+				$this->email->subject($this->options['email_subject'] .' '. $subject);
+				$this->email->message($message);
+			break;
+
+			case 'post_delete':
+				// set some variables
+				$subject = lang('email_subject_deleted_post');
+
+				// get an array of authors
+				$authors = explode(',', $data['authors']);
+
+				// find out what's the same
+				$same = array_values(array_intersect($authors, $this->session->userdata('characters')));
+
+				// figure out who it should come from
+				$from = (in_array($this->session->userdata('main_char'), $same)) ? $this->session->userdata('main_char') : $same[0];
+
+				// set who the email is coming from
+				$from_name = $this->char->get_character_name($from, true, true);
+				$from_email = $this->user->get_email_address('character', $from);
+
+				// set the content
+				$content = sprintf(
+					lang('email_content_mission_post_deleted'),
+					$data['title'],
+					$from_name
+				);
+
+				// set the email data
+				$email_data = array(
+					'email_content' => ($this->email->mailtype == 'html') ? nl2br($content) : $content
+				);
+
+				// where should the email be coming from
+				$em_loc = Location::email('write_missionpost_deleted', $this->email->mailtype);
+
+				// parse the message
+				$message = $this->parser->parse_string($em_loc, $email_data, true);
+
+				// get the email addresses
+				$emails = $this->char->get_character_emails($data['authors']);
+
+				foreach ($emails as $key => $value)
+				{
+					$pref = $this->user->get_pref('email_mission_posts_delete', $key);
+
+					if ($pref == 'y')
+					{
+						// don't do anything
+					}
+					else
+					{
+						unset($emails[$key]);
+					}
+				}
+
+				// make a string of email addresses
+				$to = implode(',', $emails);
+
+				// set the parameters for sending the email
+				$this->email->from(Util::email_sender(), $from_name);
+				$this->email->to($to);
+				$this->email->subject($this->options['email_subject'] .' '. $subject);
+				$this->email->message($message);
+			break;
+
+			case 'post_pending':
+				// get an array of authors
+				$authors = explode(',', $data['authors']);
+
+				// find out what's the same
+				$same = array_values(array_intersect($authors, $this->session->userdata('characters')));
+
+				// figure out who it should come from
+				$from = (in_array($this->session->userdata('main_char'), $same)) ? $this->session->userdata('main_char') : $same[0];
+
+				// set who the email is coming from
+				$from_name = $this->char->get_character_name($from, true, true);
+				$from_email = $this->user->get_email_address('character', $from);
+				$subject = $data['mission'] ." - ". $data['title'];
+
+				// set the content
+				$content = sprintf(
+					lang('email_content_entry_pending'),
+					lang('global_missionpost'),
+					$data['title'],
+					$from_name,
+					lang('global_missionpost'),
+					$data['content'],
+					lang('global_missionpost'),
+					site_url('login/index')
+				);
+
+				// set the email data
+				$email_data = array(
+					'email_subject' => $subject,
+					'email_from' => $from_name,
+					'email_content' => ($this->email->mailtype == 'html') ? nl2br($content) : $content
+				);
+
+				// where should the email be coming from
+				$em_loc = Location::email('entry_pending', $this->email->mailtype);
+
+				// parse the message
+				$message = $this->parser->parse_string($em_loc, $email_data, true);
+
+				// get the email addresses
+				$to = implode(',', $this->user->get_emails_with_access('manage/posts', 2));
+
+				// set the parameters for sending the email
+				$this->email->from(Util::email_sender(), $from_name);
+				$this->email->to($to);
+				$this->email->subject($this->options['email_subject'] .' '. lang('email_subject_post_pending'));
+				$this->email->message($message);
+			break;
+
+			case 'post_save':
+				// set some variables
+				$subject = $data['mission'] ." - ". $data['title'] . lang('email_subject_saved_post');
+				$mission = lang('email_content_post_mission') . $data['mission'];
+				$authors = lang('email_content_post_author') . $this->char->get_authors($data['authors'], true);
+				$timeline = lang('email_content_post_timeline') . $data['timeline'];
+				$location = lang('email_content_post_location') . $data['location'];
+
+				// get an array of authors
+				$authorsArr = explode(',', $data['authors']);
+
+				// find out what's the same
+				$same = array_values(array_intersect($authorsArr, $this->session->userdata('characters')));
+
+				// figure out who it should come from
+				$from = (in_array($this->session->userdata('main_char'), $same)) ? $this->session->userdata('main_char') : $same[0];
+
+				// set who the email is coming from
+				$from_name = $this->char->get_character_name($from, true, true);
+				$from_email = $this->user->get_email_address('character', $from);
+
+				// set the content
+				$content = sprintf(
+					lang('email_content_mission_post_saved'),
+					$data['title'],
+					site_url('login/index'),
+					$authors,
+					$mission,
+					$location,
+					$timeline,
+					$data['content']
+				);
+
+				// set the email data
+				$email_data = array(
+					'email_content' => ($this->email->mailtype == 'html') ? nl2br($content) : $content
+				);
+
+				// where should the email be coming from
+				$em_loc = Location::email('write_missionpost_saved', $this->email->mailtype);
+
+				// parse the message
+				$message = $this->parser->parse_string($em_loc, $email_data, true);
+
+				// get the email addresses
+				$emails = $this->char->get_character_emails($data['authors']);
+
+				foreach ($emails as $key => $value)
+				{
+					$pref = $this->user->get_pref('email_mission_posts_save', $key);
+
+					if ($pref == 'y')
+					{
+						// don't do anything
+					}
+					else
+					{
+						unset($emails[$key]);
+					}
+				}
+
+				// make a string of email addresses
+				$to = implode(',', $emails);
+
+				// set the parameters for sending the email
+				$this->email->from(Util::email_sender(), $from_name);
+				$this->email->to($to);
+				$this->email->subject($this->options['email_subject'] .' '. $subject);
+				$this->email->message($message);
+			break;
+		}
+
+		// send the email
+		$email = $this->email->send();
+
+		return $email;
+	}
